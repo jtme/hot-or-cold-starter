f97d751ccb0ed659e205d6a67f81ae54ce32a5ec
jtme <janis@tilla.me>
2014-06-30 15:51:07 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -1,101 +1,102 @@
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+$(document).ready(function () {
 
-  	});
+            /*--- Display information modal box ---*/
+            $(".what").click(function () {
+                $(".overlay").fadeIn(1000);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+            });
 
-  	var newGame=function(){
-  		// clear count
-  		// set rand number
-  	};
+            /*--- Hide information modal box ---*/
+            $("a.close").click(function () {
+                $(".overlay").fadeOut(1000);
+            });
 
-	var randNum = function() {
-		var n = Math.floor(Math.random() * 100);
-		console.log("Random Number is: "+n)
-		return(n);
-	};
+            var newGame = function () {
+                // clear count
+                // set rand number
+            };
 
-	var getuserNum = function(){
-    	var givenmenumber = +$('#userGuess').val();
-   		console.log(givenmenumber)
-   		return givenmenumber
-    };
+            var randNum = function () {
+                var n = Math.floor(Math.random() * 100);
+                console.log("Random Number is: " + n);
+                return (n);
+            };
 
-  	randNum();
+            var getuserNum = function () {
+                var givenmenumber = +$('#userGuess').val();
+                console.log(givenmenumber);
+                return givenmenumber;
+            };
 
-  	$("#guessButton").click(function(){
-  		getuserNum();
-  		console.log("test "+getuserNum())
-  	});
+            randNum();
 
-  	$('#userGuess').keyup(function (event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-            $('#guessButton').click();
-        };
+            $("#guessButton").click(function () {
+                getuserNum();
+                console.log("test " + getuserNum());
+            });
 
-    var temperature = function(randNum,userNum) {
-        //figure out difference
-        var temp =  Math.abs(randNum - userNum);
-    	console.log(temp)
-      
-    	 // define temp
-    	 if (5 < temp && temp <= 10){
+            $('#userGuess').keyup(function (event) {
+                if (event.keyCode == 13) {
+                    event.preventDefault();
+                    $('#guessButton').click();
+                }
+            });
 
-    	 } else if (10 < temp && temp <= 20){
+            var temperature = function (randNum, userNum) {
+                    //figure out difference
+                    var temp = Math.abs(randNum - userNum);
+                    console.log(temp);
 
-    	 } else if (20 < temp && temp <=30){
+                    // define temp
+                    if (5 < temp && temp <= 10) {
 
-    	 } else {
-                   
-         }  
+                    } else if (10 < temp && temp <= 20) {
 
-     };
+                    } else if (20 < temp && temp <= 30) {
 
-    	 //set temp
+                    } else {
 
-    	 //reset
+                    }
 
-})
+                };
+
+                //set temp
+
+                //reset
+
+            });
 
 
-       // if (givennumber >= 1 && givennumber <= 100) {
-       //         // figure out how warm
-        //        for (var n = 1; n <= givennumber; n++) {
-         //           if (n % 15 === 0) {
-          //              addtolist("fizz buzz");
-          //           } else if (n % 3 === 0){
-          //              addtolist("fizz");
-          //           } else if (n % 5 === 0){ 
-          //              addtolist("buzz");
-          //           } else {
-          //              addtolist(n);
-         //            }          
-         //       }
-        //}else{
-        //   $("#feedback").text("nope!");
-        //};
-    // };
+            // if (givennumber >= 1 && givennumber <= 100) {
+            //         // figure out how warm
+            //        for (var n = 1; n <= givennumber; n++) {
+            //           if (n % 15 === 0) {
+            //              addtolist("fizz buzz");
+            //           } else if (n % 3 === 0){
+            //              addtolist("fizz");
+            //           } else if (n % 5 === 0){ 
+            //              addtolist("buzz");
+            //           } else {
+            //              addtolist(n);
+            //            }          
+            //       }
+            //}else{
+            //   $("#feedback").text("nope!");
+            //};
+            // };
 
-  	// function to tell how hot or cold is it
-  	// 50 ice cold, 30 cold, 20 warm, 10 hot, vary hot 
-  	//	id="guessButton"
-  	//  div#feedback text feedback
-  	// span#count cont
+            // function to tell how hot or cold is it
+            // 50 ice cold, 30 cold, 20 warm, 10 hot, vary hot 
+            //	id="guessButton"
+            //  div#feedback text feedback
+            // span#count cont
 
-  	// ok click start game
-	// $("a.close").click(function(){
-  	// 	$(".overlay").fadeOut(1000);
-  	// });
+            // ok click start game
+            // $("a.close").click(function(){
+            // 	$(".overlay").fadeOut(1000);
+            // });
 
-	// update count
+            // update count
 
-	// function difference(n, m){
-    // return Math.abs(n - m)
+            // function difference(n, m){
+            // return Math.abs(n - m)
