130dd880e7d36161efc855eb6ebfae99881f0cae
jtme <janis@tilla.me>
2014-07-21 16:02:38 +0300

    cleanup!

--- js/app.js
+++ js/app.js
@@ -59,13 +59,6 @@ $(document).ready(function () {
                 newGame();
             });
 
-            //$('#userGuess').keyup(function (event) {
-             //   if (event.keyCode == 13) {
-             //       event.preventDefault();
-             //       $('#guessButton').click();
-             //  }
-            //});
-
             var temperature = function (randNum, userNum) {
                     //figure out difference
                     var temp = Math.abs(randNum - userNum);
@@ -86,46 +79,5 @@ $(document).ready(function () {
                     } else {
 
                     }
-
                 };
-
-                //set temp
-
-                //reset
-
             });
-
-
-            // if (givennumber >= 1 && givennumber <= 100) {
-            //         // figure out how warm
-            //        for (var n = 1; n <= givennumber; n++) {
-            //           if (n % 15 === 0) {
-            //              addtolist("fizz buzz");
-            //           } else if (n % 3 === 0){
-            //              addtolist("fizz");
-            //           } else if (n % 5 === 0){ 
-            //              addtolist("buzz");
-            //           } else {
-            //              addtolist(n);
-            //            }          
-            //       }
-            //}else{
-            //   $("#feedback").text("nope!");
-            //};
-            // };
-
-            // function to tell how hot or cold is it
-            // 50 ice cold, 30 cold, 20 warm, 10 hot, vary hot 
-            //	id="guessButton"
-            //  div#feedback text feedback
-            // span#count cont
-
-            // ok click start game
-            // $("a.close").click(function(){
-            // 	$(".overlay").fadeOut(1000);
-            // });
-
-            // update count
-
-            // function difference(n, m){
-            // return Math.abs(n - m)
